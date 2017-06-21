e538a4d9c90e6deec2804e23e92f11e1118285bb
AleksandrMisko <misik.sashamiskoblr@gmail.com>
2017-06-21 08:14:44 +0200

    add DEV-1 files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialOutputOfMultipleNumbers_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number_counter = 0; number_counter <= 100; number_counter++)
+            {
+                String Output = (number_counter % 3 == 0 ? "3*" + number_counter / 3 : number_counter.ToString());
+                Console.Write(Output + ";\n");
+            }
+            Console.WriteLine("The program has completed work. Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
