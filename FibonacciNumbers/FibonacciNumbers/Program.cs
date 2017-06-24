fc6ddc8f9fff46a8b11985fc8b3a290bc1f9cb31
AleksandrMisko <misik.sashamiskoblr@gmail.com>
2017-06-24 15:01:22 +0300

    Add main code

--- FibonacciNumbers/FibonacciNumbers/Program.cs
+++ FibonacciNumbers/FibonacciNumbers/Program.cs
@@ -16,6 +16,29 @@ namespace FibonacciNumbers
                     int Number = int.Parse(Console.ReadLine());
                     if (Number >= 0)
                     {
+                        int FibonacciNumb1 = 0, FibonacciNumb2 = 1, FibonacciNumbN = 1;
+                        bool checkFibonacci = false;
+                        for (int i = 0; i < Number; i++)
+                        {
+                            FibonacciNumb1 = FibonacciNumb2;
+                            FibonacciNumb2 = FibonacciNumbN;
+                            FibonacciNumbN = FibonacciNumb1 + FibonacciNumb2;
+                            if (Number == FibonacciNumbN || Number == 1)
+                            {
+                                checkFibonacci = true;
+                                break;
+                            }
+                        }
+                        if (checkFibonacci)
+                        {
+                            Console.WriteLine("This is the Fibonacci number.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not the Fibonacci number.");
+                        }
+                        Console.WriteLine("\nPress any key to exit.");
+                        Console.ReadKey();
                     }
                     else
                     {
