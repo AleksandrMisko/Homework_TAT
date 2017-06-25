731484d9abcb5e281be3f25f1b35f32692d8a97a
AleksandrMisko <misik.sashamiskoblr@gmail.com>
2017-06-25 16:48:04 +0300

    Class name

--- NumSeqCheck/Program.cs
+++ NumSeqCheck/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace NumSeqCheck
 {
-    class Program
+    class NumericalSequenceCheck
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
