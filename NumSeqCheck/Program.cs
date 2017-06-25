56585159726cb3981cfeea7ca0ddb064e06d404a
AleksandrMisko <misik.sashamiskoblr@gmail.com>
2017-06-25 17:22:06 +0300

    Add main code

--- NumSeqCheck/Program.cs
+++ NumSeqCheck/Program.cs
@@ -13,6 +13,36 @@ namespace NumSeqCheck
                 Console.Write("Enter size of the sequence = ");
                 String sizeArr = Console.ReadLine();
                 int size = int.Parse(sizeArr);
+                int[] Aarray = new int[size];
+                string strAr;
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write("Enter " + (i + 1) + " element of the sequence = ");
+                    strAr = Console.ReadLine();
+                    Aarray[i] = int.Parse(strAr);
+                }
+                bool cheсk = true;
+                for (int i = 1; i < size; i++)
+                {
+                    if (Aarray[i] >= Aarray[i - 1])
+                    {
+                        cheсk = true;
+                    }
+                    if (Aarray[i] < Aarray[i - 1])
+                    {
+                        cheсk = false;
+                        break;
+                    }
+                }
+                if (cheсk == false)
+                {
+                    Console.WriteLine("This sequence is decreasing ");
+                }
+                else
+                {
+                    Console.WriteLine("This sequence is nondecreasing ");
+                }
+                Console.ReadKey();
                 value = false;
             }
         }
