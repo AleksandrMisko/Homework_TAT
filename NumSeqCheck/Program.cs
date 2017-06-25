b854a9a2b598ec6ad3a57a5e663dc4178a0ddcb2
AleksandrMisko <misik.sashamiskoblr@gmail.com>
2017-06-25 18:04:19 +0300

    Add exception

--- NumSeqCheck/Program.cs
+++ NumSeqCheck/Program.cs
@@ -10,47 +10,55 @@ namespace NumSeqCheck
             bool value = true;
             while (value)
             {
-                Console.Write("Enter size of the sequence = ");
-                String sizeArr = Console.ReadLine();
-                int size = int.Parse(sizeArr);
-                int[] Aarray = new int[size];
-                string strAr;
-                if (size <= 1)
+                try
                 {
-                    Console.WriteLine("Are you seriously? Let's enter the size of the SEQUENCE.");
-                    continue;
-                }
-                else
-                {
-                    for (int i = 0; i < size; i++)
+                    Console.Write("Enter size of the sequence = ");
+                    String sizeArr = Console.ReadLine();
+                    int size = int.Parse(sizeArr);
+                    int[] Aarray = new int[size];
+                    string strAr;
+                    if (size <= 1)
                     {
-                        Console.Write("Enter " + (i + 1) + " element of the sequence = ");
-                        strAr = Console.ReadLine();
-                        Aarray[i] = int.Parse(strAr);
-                    }
-                    bool cheсk = true;
-                    for (int i = 1; i < size; i++)
-                    {
-                        if (Aarray[i] >= Aarray[i - 1])
-                        {
-                            cheсk = true;
-                        }
-                        if (Aarray[i] < Aarray[i - 1])
-                        {
-                            cheсk = false;
-                            break;
-                        }
-                    }
-                    if (cheсk == false)
-                    {
-                        Console.WriteLine("This sequence is decreasing ");
+                        Console.WriteLine("Are you seriously? Let's enter size of the SEQUENCE.");
+                        continue;
                     }
                     else
                     {
-                        Console.WriteLine("This sequence is nondecreasing ");
+                        for (int i = 0; i < size; i++)
+                        {
+                            Console.Write("Enter " + (i + 1) + " element of the sequence = ");
+                            strAr = Console.ReadLine();
+                            Aarray[i] = int.Parse(strAr);
+                        }
+                        bool cheсk = true;
+                        for (int i = 1; i < size; i++)
+                        {
+                            if (Aarray[i] >= Aarray[i - 1])
+                            {
+                                cheсk = true;
+                            }
+                            if (Aarray[i] < Aarray[i - 1])
+                            {
+                                cheсk = false;
+                                break;
+                            }
+                        }
+                        if (cheсk == false)
+                        {
+                            Console.WriteLine("This sequence is decreasing ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("This sequence is nondecreasing ");
+                        }
+                        Console.ReadKey();
+                        value = false;
                     }
-                    Console.ReadKey();
-                    value = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("It is not a number. Try again.");
+                    continue;
                 }
             }
         }
